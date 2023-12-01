6ce1e1af05be0720aa045d77e6d682c27ce88171
Senevri <esa.karjalainen@gmail.com>
2023-12-01 12:15:15 +0200

    black

--- vsurvivor/python/arcade/test_arcade.py
+++ vsurvivor/python/arcade/test_arcade.py
@@ -27,11 +27,11 @@ class Player(arcade.Sprite):
         self.center_y = y
         self.change_x = 0
         self.change_y = 0
-        
+
     def __repr__(self):
         return (str((self.center_x, self.center_y, self.change_x, self.change_y)))
 
-@dataclass        
+@dataclass
 class GameState:
     player: Optional[Player] = None
     player_list: arcade.SpriteList = arcade.SpriteList()
@@ -47,31 +47,31 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.color.DARK_GREEN)
         self.lastupdate = 0
         self.terminated = False
-    
+
 
     def setup(self, state=None):
         self.state.player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-        if not self.state.player_list: 
+        if not self.state.player_list:
             self.state.player_list = arcade.SpriteList()
-        if self.state.player not in self.state.player_list: 
+        if self.state.player not in self.state.player_list:
             self.state.player_list.append(self.state.player)
-        
+
     def on_key_release(self, key, modifiers):
         log_key(key, modifiers)
-        
+
         key_lookup_x = [
             arcade.key.LEFT,
             arcade.key.RIGHT,
         ]
         key_lookup_y = [
             arcade.key.UP,
-            arcade.key.DOWN            
+            arcade.key.DOWN
         ]
 
         if key in key_lookup_x:
             self.state.player.change_x = 0
         if key in key_lookup_y:
-            self.state.player.change_y = 0 
+            self.state.player.change_y = 0
 
         if key in key_lookup_x or key in key_lookup_y:
             self.keys_down.remove(key)
@@ -80,7 +80,7 @@ class MyGame(arcade.Window):
 
     def on_key_press(self, key, modifiers):
         log_key(key, modifiers)
-        
+
         key_lookup = {
             arcade.key.LEFT: (-5, 0),
             arcade.key.RIGHT: (5, 0),
@@ -88,28 +88,28 @@ class MyGame(arcade.Window):
             arcade.key.DOWN: (0, -5),
             arcade.key.ESCAPE: "close"
         }
-        
-        if key in key_lookup:            
+
+        if key in key_lookup:
             action = key_lookup[key]
             if action == "close":
                 self.terminated = True
                 arcade.close_window()
-            elif isinstance(key_lookup[key], tuple): 
+            elif isinstance(key_lookup[key], tuple):
                 self.keys_down.add(key)
                 self.state.player.change_x, self.state.player.change_y = action
         #self.state.player.center_x += self.state.player.change_x
         #self.state.player.center_y += self.state.player.change_y
-                    
+
     def on_draw(self):
         arcade.start_render()
-        try: 
+        try:
             self.state.player_list.draw()
         except Exception as ex:
             logger.error(ex)
             logger.warn(self.state)
             arcade.close_window()
 
-    
+
     def on_update(self, delta_time):
         self.lastupdate += delta_time
         if self.lastupdate > 5:
@@ -118,14 +118,14 @@ class MyGame(arcade.Window):
                 arcade.close_window()
         #logger.warning(self.state.player)
 
-        if self.keys_down: 
+        if self.keys_down:
             if arcade.key.LEFT in self.keys_down or  arcade.key.RIGHT in self.keys_down:
                 self.state.player.center_x += self.state.player.change_x
             if arcade.key.UP in self.keys_down or  arcade.key.DOWN in self.keys_down:
                 self.state.player.center_y += self.state.player.change_y
 
         self.state.player_list.update()
-    
+
 def main():
     game = MyGame()
     while not game.terminated:
@@ -133,7 +133,7 @@ def main():
         arcade.run()
         state = game.state
         game = MyGame(state)
-    
+
 
 
 if __name__ == "__main__":
--- worm/python/libs/worm.py
+++ worm/python/libs/worm.py
@@ -3,6 +3,7 @@ from typing import Optional
 import pygame
 from pygame import Surface
 from time import sleep
+
 # from pygame.locals import (
 
 # )
@@ -14,15 +15,16 @@ except ImportError:
 
 logger = log.getLogger(__name__)
 
+
 @dataclass
-class WormState():
-    x:int
-    y:int
-    size:int
-    color:tuple
+class WormState:
+    x: int
+    y: int
+    size: int
+    color: tuple
 
     def tuple(self):
-        return(self.x, self.y, self.size, self.color)
+        return (self.x, self.y, self.size, self.color)
 
     def __eq__(self, cmp):
         logger.warn("cmp")
@@ -31,13 +33,13 @@ class WormState():
         # )
         return self.tuple() != cmp.tuple()
 
+
 @dataclass
-class State():
+class State:
     window_caption: str = ""
     times: int = 0
     quit_game: bool = False
-    worm = WormState(100,100,24, (255,255,255)).tuple()
-
+    worm = WormState(100, 100, 24, (255, 255, 255)).tuple()
 
 
 class PyGame:
@@ -52,7 +54,6 @@ class PyGame:
         self.size = self.weight, self.height = 800, 800
         self.on_init()
 
-
     def on_init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE)
@@ -61,7 +62,7 @@ class PyGame:
         self.curloop = 0
 
     def on_event(self, event):
-        #logger.info(f"Event! {event}")
+        # logger.info(f"Event! {event}")
         if event.type == pygame.QUIT:
             self._running = False
 
@@ -76,36 +77,36 @@ class PyGame:
             self.curloop = 0
             self.state.worm = _worm.tuple()
             self.update_display = True
-        #logger.info(f"Loop!")
+        # logger.info(f"Loop!")
 
     def draw_worm(self):
         worm_s = WormState(*self.state.worm)
-        pygame.draw.rect(self._display_surf, worm_s.color, (worm_s.x, worm_s.y, worm_s.size, worm_s.size))
+        pygame.draw.rect(
+            self._display_surf, worm_s.color, (worm_s.x, worm_s.y, worm_s.size, worm_s.size)
+        )
 
     def on_render(self):
         if self.update_display:
-            self._display_surf.fill((0,0,0))
+            self._display_surf.fill((0, 0, 0))
             self.update_display = False
         self.draw_worm()
-        #logger.info("render")
+        # logger.info("render")
         pygame.display.update()
-
 
     def on_cleanup(self):
         self.state.times += 1
         pygame.quit()
 
     def on_execute(self):
-        #logger.info("running")
+        # logger.info("running")
         for event in pygame.event.get():
             self.on_event(event)
         self.on_loop()
         self.on_render()
-        #Reload libs here
+        # Reload libs here
 
     def update(self):
         self.on_execute()
         logger.warn(self.state.worm)
         # logger.warn(self.curloop)
         return self.is_running()
-
