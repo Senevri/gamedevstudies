c365bdb56e985dd25518d98ecb1ec90e6edec5df
Esa Karjalainen <esa.karjalainen@botlabs.fi>
2023-12-27 16:36:42 +0200

    Add sample scaling canvas

---
+++ pointnclickadventure/javascript/script.js
@@ -0,0 +1,55 @@
+const canvas = document.getElementById('myCanvas');
+const context = canvas.getContext('2d');
+
+// Set the fixed canvas size
+const fixedWidth = 320; // Set your desired fixed width
+const fixedHeight = 240; // Set your desired fixed height
+canvas.width = fixedWidth;
+canvas.height = fixedHeight;
+
+// Function to update the canvas size and draw on the canvas
+function updateCanvas() {
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
+    // Calculate the scaling factors
+    const scaleWidth = windowWidth / fixedWidth;
+    const scaleHeight = windowHeight / fixedHeight;
+
+    // Use the minimum scaling factor to maintain the aspect ratio
+    const scale = Math.min(scaleWidth, scaleHeight);
+
+    // Update the canvas size based on the scaling factor
+    canvas.width = fixedWidth * scale;
+    canvas.height = fixedHeight * scale;
+
+    // Clear the canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+// Draw a box with corner markers
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = 'rgba(255, 0, 0, 0.3)';
+    context.fillRect(50, 50, canvas.width - 100, canvas.height - 100);
+    // Draw corner markers
+    context.fillStyle = '#000';
+    context.fillRect(50 - 5, 50 - 5, 10, 10);
+    context.fillRect(canvas.width - 50 - 5, 50 - 5, 10, 10);
+    context.fillRect(canvas.width - 50 - 5, canvas.height - 50 - 5, 10, 10);
+    context.fillRect(50 - 5, canvas.height - 50 - 5, 10, 10);
+
+
+    // Draw a single pixel at the center
+    const centerX = Math.floor(canvas.width / 2);
+    const centerY = Math.floor(canvas.height / 2);
+    const pixelSize = 4*scale;
+
+    context.fillStyle = '#000'; // Set color to black
+    context.fillRect(centerX, centerY, pixelSize, pixelSize);
+    context.font = "16px Cascadia Code"
+    context.fillText(scale, 100,100)
+
+}
+
+// Call the function initially and whenever the window is resized
+updateCanvas();
+window.addEventListener('resize', updateCanvas);
